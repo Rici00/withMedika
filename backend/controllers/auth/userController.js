a98a94c460c6e069fe6ad7b157c5c5a4f1d55d1e
Rici00 <richardharryson@mail.ugm.ac.id>
2023-08-03 13:13:00 +0800

    set login

--- backend/controllers/auth/userController.js
+++ backend/controllers/auth/userController.js
@@ -1,47 +1,56 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken'); // Import jsonwebtoken
 const usersTable = require('../../models/userModel')
 
-const register = async (req, res) => {
-	const { first_name, last_name, email, password, about_us } = req.body;
-	const checkIfUserExist = await usersTable.findOne({ email: email })
-	try {
-		// ✅ User sudah terdaftar ??
-		if ( checkIfUserExist ) {
-			return res.status(200).json({ success: false, message : "user is already exist"})
-		} else {
-			// ✅ Hash password dan store ke db
-			const hashpw = await bcrypt.hash(password, 10)
-			const user = new usersTable({ first_name, last_name, email, password: hashpw, about_us })
-			user.save()
-			return res.status(200).json({ success: true, message : "user is created"})
-		}
-	} catch ( e ) {
-		// ✅ Handle error
-		return res.status(404).json({ errors : "errors"})
-	}
+const register = async(req, res) => {
+    const { first_name, last_name, email, password, about_us } = req.body;
+    const checkIfUserExist = await usersTable.findOne({ email: email })
+    try {
+        // ✅ User sudah terdaftar ??
+        if (checkIfUserExist) {
+            return res.status(200).json({ success: false, message: "user is already exist" })
+        } else {
+            // ✅ Hash password dan store ke db
+            const hashpw = await bcrypt.hash(password, 10)
+            const user = new usersTable({ first_name, last_name, email, password: hashpw, about_us })
+            user.save()
+            return res.status(200).json({ success: true, message: "user is created" })
+        }
+    } catch (e) {
+        // ✅ Handle error
+        return res.status(404).json({ errors: "errors" })
+    }
 }
 
-const login = async (req, res ) => {
-	const { email , password } = req.body;
+const login = async(req, res) => {
+    const { email, password } = req.body;
     let existingUser;
-    try{
-    	// ✅ Email user ada di db ??
-     	existingUser = await usersTable.findOne({email})
-    } catch( e ) {
-     	console.log("Error");
+    try {
+        existingUser = await usersTable.findOne({ email });
+    } catch (e) {
+        console.log("Error");
     }
-    // ✅ User tidak ada di db ??
-    if(existingUser == null){
-        return res.status(404).json( { status: false, message : "User is not found" } )
+
+    if (existingUser == null) {
+        return res.status(404).json({ status: false, message: "User is not found" });
     }
-    // ✅ Cek password yang sudah di hash 
-    const isPasswordCorrect = await bcrypt.compare( password, existingUser.password );
-    if( isPasswordCorrect ) {
-    	// ✅ Sukses login ??
-    	return res.status(200).json( { status: true, message: "Succesfully login!", user: existingUser })
+
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if (isPasswordCorrect) {
+        // If password is correct, generate a token
+        const token = jwt.sign({ userId: existingUser._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h', // Token expiration time (optional)
+        });
+
+        return res.status(200).json({
+            status: true,
+            message: "Successfully login!",
+            user: existingUser,
+            token: token, // Include the generated token in the response
+        });
     }
-    // ✅ Gagal login ??
-    return res.status(400).json( { status: false, message: "Incorrect Password!" })
-    
-}
-module.exports = { register, login };
+
+    return res.status(400).json({ status: false, message: "Incorrect Password!" });
+};
+
+module.exports = { register,  login  };
--- frontend/src/pages/indexlogin.jsx
+++ frontend/src/pages/indexlogin.jsx
@@ -1,13 +1,28 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-import { Calcu } from '@/components/Calcu'
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
-import { Hero } from '@/components/Hero'
-import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Calcu } from '@/components/Calcu';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { Hero } from '@/components/Hero';
+import { PrimaryFeatures } from '@/components/PrimaryFeatures';
+import { SecondaryFeatures } from '@/components/SecondaryFeatures';
 
 export default function Home() {
+  const router = useRouter();
+  let user = {};
+  if (typeof window !== 'undefined') {
+    user = JSON.parse(localStorage.getItem('user'));
+  }
+
+  useEffect(() => {
+    // Redirect to initial page if no user data available
+    if (!user || !user.email) {
+      router.push('/'); // Replace '/' with the route to your initial page
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -25,5 +40,5 @@ export default function Home() {
       </main>
       <Footer />
     </>
-  )
+  );
 }
